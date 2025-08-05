12d3fc888754668a3181c156af90b4a1a3f3e94b
capistolero <gabriel.capistolero@gmail.com>
2025-08-05 06:18:33 -0300

    my-pc-is-dying

--- lua/Capistolero/lazy.lua
+++ lua/Capistolero/lazy.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup(
     {
         { import = "Capistolero.plugins" },
-        
+        { import = "Capistolero.plugins.lsp" },
     },
     {
         checker = {
